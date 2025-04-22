34ba5b86390f27997fc49bce70f90b4b6c1af891
Mehdi Mohseni <wuka.sec@gmail.com>
2025-04-22 12:57:42 +0330

    Add: Example for OC Principle

---
+++ OCP/FirstDraft.cs
@@ -0,0 +1,34 @@
+﻿namespace OCP;
+
+class FirstDraft
+{
+    public double GetInvoiceDiscount(double amount, InvoiceType invoiceType)
+    {
+        double finalAmount = default;
+
+        if (invoiceType is InvoiceType.FinalInvoice)
+        {
+            finalAmount = amount - 100;
+        }
+        else if (invoiceType is InvoiceType.ProposedInvoice)
+        {
+            finalAmount = amount - 50;
+        }
+
+        return finalAmount;
+    }
+
+    // code is more clean with switch but still break the OC principle
+    public double GetInvoiceDiscountWithSwitch(double amount, InvoiceType invoiceType) => invoiceType switch
+    {
+        InvoiceType.FinalInvoice => amount - 100,
+        InvoiceType.ProposedInvoice => amount - 50,
+        _ => 0
+    };
+
+    public enum InvoiceType
+    {
+        FinalInvoice,
+        ProposedInvoice
+    }
+}
---
+++ OCP/Refactored.cs
@@ -0,0 +1,24 @@
+﻿namespace OCP;
+
+abstract class Invoice
+{
+    public abstract double GetDiscount(double amount);
+}
+
+class FinalInvoice : Invoice
+{
+    public override double GetDiscount(double amount) 
+        => amount - 100;
+}
+
+class ProposedInvoice : Invoice
+{
+    public override double GetDiscount(double amount) 
+        => amount - 50;
+}
+
+class InvoiceProcessor
+{
+    public double GetInvoiceDiscount(double amount, Invoice invoice) 
+        => invoice.GetDiscount(amount);
+}
