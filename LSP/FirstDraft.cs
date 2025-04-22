3cb8c8ac09767c875730c3765b03cc34a3c64092
Mehdi Mohseni <wuka.sec@gmail.com>
2025-04-22 13:14:07 +0330

    Add: Example for LS Principle

---
+++ LSP/FirstDraft.cs
@@ -0,0 +1,22 @@
+﻿namespace LSP;
+
+public class Rectangle
+{
+    public virtual double Width { get; set; }
+    public virtual double Height { get; set; }
+
+    public double GetArea() => Width * Height;
+}
+
+public class Square : Rectangle
+{
+    public override double Width
+    {
+        set { base.Width = base.Height = value; }
+    }
+
+    public override double Height
+    {
+        set { base.Width = base.Height = value; }
+    }
+}
---
+++ LSP/Refactored.cs
@@ -0,0 +1,33 @@
+﻿namespace LSPRef;
+
+interface IShape
+{
+    double GetArea();
+}
+
+// use interfaces 
+public class Rectangle : IShape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double GetArea() => Width * Height;
+}
+
+public class Square : IShape
+{
+    public double Side { get; set; }
+
+    public Square(double side)
+    {
+        Side = side;
+    }
+
+    public double GetArea() => Side * Side;
+}
