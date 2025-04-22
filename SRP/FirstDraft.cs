4c681dba37a942a9e0f3b0532acda84177a96d3c
Mehdi Mohseni <wuka.sec@gmail.com>
2025-04-22 12:35:31 +0330

    Add: Examples for SR Principle

---
+++ SRP/FirstDraft.cs
@@ -0,0 +1,32 @@
+﻿namespace SRP;
+
+internal class FirstDraft
+{
+    static void ActivateAccount()
+    {
+        // get email from user
+        var userEmail = Console.ReadLine()!;
+
+        try
+        {
+            SendEmail(userEmail);
+        }
+        catch (Exception e)
+        {
+            // if we can't send email we log it into a file 
+
+            var dateTime = DateTime.Now;
+            var logPath = $"/logs/log-{dateTime}.txt";
+
+            using StreamWriter writer = new(logPath);
+            writer.Write(e.Data);
+            writer.Close();
+            throw;
+        }
+    }
+
+    static void SendEmail(string inputEmail)
+    {
+        // logic for send email to {inputEmail} 
+    }
+}
---
+++ SRP/Refactored.cs
@@ -0,0 +1,40 @@
+﻿namespace SRP;
+
+internal class Refactored
+{
+    void ActivateAccount()
+    {
+        // get email from user
+        var userEmail = Console.ReadLine()!;
+
+        try
+        {
+            SendEmail(userEmail);
+        }
+        catch (Exception e)
+        {
+            // use logger class for seperate the responsibilities
+            Logger.Log(e);
+            throw;
+        }
+    }
+
+    static void SendEmail(string inputEmail)
+    {
+        // logic for send email to {inputEmail} 
+    }
+}
+
+// it's better to put this class into a seperate file
+internal class Logger
+{
+    public static void Log(Exception e)
+    {
+        var dateTime = DateTime.Now;
+        var logPath = $"/logs/log-{dateTime}.txt";
+
+        using StreamWriter writer = new(logPath);
+        writer.Write(e.Data);
+        writer.Close();
+    }
+}
