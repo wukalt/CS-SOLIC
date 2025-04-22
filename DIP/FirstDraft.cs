08bc866eb00281bd7207489e4942ca89296abf9d
Mehdi Mohseni <wuka.sec@gmail.com>
2025-04-22 14:45:52 +0330

    Add: Example for DI Principle

---
+++ DIP/FirstDraft.cs
@@ -0,0 +1,28 @@
+﻿namespace DIP;
+
+public class Logger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine("Logging: " + message);
+    }
+}
+
+public class UserService
+{
+    /* it's depend on logger class and if we have 
+     another logger for this class we need to rewrite this */
+    private readonly Logger _logger;
+
+    public UserService(Logger logger)
+    {
+        _logger = logger;
+    }
+
+    public void RegisterUser(string username)
+    {
+        Console.WriteLine($"User {username} registered.");
+
+        _logger.Log("User registered: " + username);
+    }
+}
---
+++ DIP/Refactored.cs
@@ -0,0 +1,39 @@
+﻿namespace DIPRef;
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine("Logging: " + message);
+    }
+}
+
+public class FileLogger : ILogger
+{
+    public void Log(string message)
+    {
+        File.WriteAllText("log.txt", message);
+    }
+}
+
+public class UserService
+{
+    private readonly ILogger _logger;
+
+    /* it's depend on abstraction not a class */
+    public UserService(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void RegisterUser(string username)
+    {
+        Console.WriteLine($"User {username} registered.");
+        _logger.Log("User registered: " + username);
+    }
+}
