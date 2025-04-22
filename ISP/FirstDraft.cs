9d77045886d263b2fe24d12060389afbd341843b
Mehdi Mohseni <wuka.sec@gmail.com>
2025-04-22 13:18:12 +0330

    Add: Example for IS Principle

---
+++ ISP/FirstDraft.cs
@@ -0,0 +1,20 @@
+﻿namespace ISP;
+
+public interface IMachine
+{
+    void Print(string document);
+    void Scan(string document);
+}
+
+public class Printer : IMachine
+{
+    public void Print(string document)
+    {
+        Console.WriteLine("Printing: " + document);
+    }
+
+    public void Scan(string document)
+    {
+        throw new NotImplementedException("This printer does not support scanning!");
+    }
+}
---
+++ ISP/Refactored.cs
@@ -0,0 +1,32 @@
+﻿namespace ISPRef;
+
+public interface IPrinter
+{
+    void Print(string document);
+}
+
+public interface IScanner
+{
+    void Scan(string document);
+}
+
+public class Printer : IPrinter
+{
+    public void Print(string document)
+    {
+        Console.WriteLine("Printing: " + document);
+    }
+}
+
+public class MultiFunctionPrinter : IPrinter, IScanner
+{
+    public void Print(string document)
+    {
+        Console.WriteLine("Printing: " + document);
+    }
+
+    public void Scan(string document)
+    {
+        Console.WriteLine("Scanning: " + document);
+    }
+}
